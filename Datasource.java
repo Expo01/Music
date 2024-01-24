a52de0c1baf7ac687164cd19851380aabe181de2
Ryan <crdailey01@gmail.com>
2024-01-23 22:36:19 -0800

    count, creating view...

--- Datasource.java
+++ Datasource.java
@@ -62,7 +62,36 @@ public class Datasource {
             " ORDER BY " + TABLE_ARTISTS + "." + COLUMN_ARTIST_NAME + ", " + TABLE_ALBUMS + "." + COLUMN_ALBUM_NAME
             + " COLLATE NOCASE ";
 
+    public static final String TABLE_ARTIST_SONG_VIEW = "artist_list";
+
+    public static final String CREATE_ARTIST_FOR_SONG_VIEW = "CREATE VIEW IF NOT EXISTS " +
+            TABLE_ARTIST_SONG_VIEW + " AS SELECT " + TABLE_ARTISTS + "." + COLUMN_ARTIST_NAME + ", " +
+            TABLE_ALBUMS + "." + COLUMN_ALBUM_NAME + " AS " + COLUMN_SONG_ALBUM + ", " +
+            TABLE_SONGS + "." + COLUMN_SONG_TRACK + ", " + TABLE_SONGS + "." + COLUMN_SONG_TITLE +
+            " FROM " + TABLE_SONGS +
+            " INNER JOIN " + TABLE_ALBUMS + " ON " + TABLE_SONGS +
+            "." + COLUMN_SONG_ALBUM + " = " + TABLE_ALBUMS + "." + COLUMN_ALBUM_ID +
+            " INNER JOIN " + TABLE_ARTISTS + " ON " + TABLE_ALBUMS + "." + COLUMN_ALBUM_ARTIST +
+            " = " + TABLE_ARTISTS + "." + COLUMN_ARTIST_ID +
+            " ORDER BY " +
+            TABLE_ARTISTS + "." + COLUMN_ARTIST_NAME + ", " +
+            TABLE_ALBUMS + "." + COLUMN_ALBUM_NAME + ", " +
+            TABLE_SONGS + "." + COLUMN_SONG_TRACK;
+
+    public static final String QUERY_VIEW_SONG_INFO =  "SELECT " + COLUMN_ARTIST_NAME + ", " +
+            COLUMN_SONG_ALBUM + ", " + COLUMN_SONG_TRACK + " FROM " + TABLE_ARTIST_SONG_VIEW +
+            " WHERE " + COLUMN_SONG_TITLE + " = \"";
+
+    public static final String QUERY_VIEW_SONG_INFO_PREP = "SELECT " + COLUMN_ARTIST_NAME + ", " +
+            COLUMN_SONG_ALBUM + ", " + COLUMN_SONG_TRACK + " FROM " + TABLE_ARTIST_SONG_VIEW +
+            " WHERE " + COLUMN_SONG_TITLE + " = ?";
+
+   private PreparedStatement querySongInfoView;
+
     private Connection conn;
+
+
+    // --------------------- METHODS ----------------------------
 
     public boolean open() {
         try {
@@ -180,7 +209,9 @@ public class Datasource {
 
         StringBuilder sb = new StringBuilder(QUERY_ARTIST_FOR_SONG_START);
         sb.append(songName);
-        sb.append("\"");
+        sb.append("\""); // escape sequence with " negates the " as part of the SQL statement. there is one at the end
+        // of the Query artist for song start String which handles the first " before the song name, then we append
+        // another \" to handle the second " after the song name
 
         if(sortOrder != ORDER_BY_NONE) {
             sb.append(QUERY_ARTIST_FOR_SONG_SORT);
@@ -198,6 +229,90 @@ public class Datasource {
 
             List<SongArtist> songArtists = new ArrayList<>();
 
+            while(results.next()) { // SongArtist class allows creation of new object with fields for song name, album
+                // name and artist name
+                SongArtist songArtist = new SongArtist();
+                songArtist.setArtistName(results.getString(1));
+                songArtist.setAlbumName(results.getString(2));
+                songArtist.setTrack(results.getInt(3));
+                songArtists.add(songArtist);
+            }
+
+            return songArtists; // List returned but not printed here
+        } catch(SQLException e) {
+            System.out.println("Query failed: " + e.getMessage());
+            return null;
+        }
+    } // passing a known song name, the
+    // will create a view of track #, album name and artist name using album ID to join songs and albums table
+    // and artist ID to join albums and artist tables
+
+    public void querySongsMetadata() {
+        String sql = "SELECT * FROM " + TABLE_SONGS;
+
+        try (Statement statement = conn.createStatement();
+             ResultSet results = statement.executeQuery(sql)) {
+
+            ResultSetMetaData meta = results.getMetaData(); //ResultSetMetaData is an object that can be used to get
+            // information about the types and properties of the columns in a ResultSet object
+            int numColumns = meta.getColumnCount();
+            for(int i=1; i<= numColumns; i++) { // indices in table columns start at 1
+                System.out.format("Column %d in the songs table is names %s\n", i, meta.getColumnName(i));
+                // \n here is just a normal escape for a new line
+                // 'souf' format allows passing of %d for 'i' and %s for 'meta.getColumnName(i)' for int and string
+                // where 'i' is incremented so prints column index and name for all existing columns in songs table
+            }
+        } catch(SQLException e) {
+            System.out.println("Query failed: " + e.getMessage());
+        }
+    } // prints column titles from the songs table
+
+
+    public int getCount(String table) {
+        String sql = "SELECT COUNT(*) AS count FROM " + table; // 'AS' used to assign name to resulting column...
+        try(Statement statement = conn.createStatement();
+            ResultSet results = statement.executeQuery(sql)) {
+
+            int count = results.getInt("count");
+//            int count = results.getInt(1); only one column from result set, returns and assigns that int val
+
+            System.out.format("Count = %d\n", count);
+            return count;
+        } catch(SQLException e) {
+            System.out.println("Query failed: " + e.getMessage());
+            return -1;
+        }
+    } // will return the number of rows in specified table
+
+    public boolean createViewForSongArtists() {
+
+        try(Statement statement = conn.createStatement()) {
+
+            statement.execute(CREATE_ARTIST_FOR_SONG_VIEW);
+            return true;
+
+        } catch(SQLException e) {
+            System.out.println("Create View failed: " + e.getMessage());
+            return false;
+        }
+    } // creates a view which is added to the database and will be viewable
+    // in DB browser with a different icon indicating it is a view not a table. This particular view will have columns:
+    // artist name, album name, album track #, song name
+    // view will be queriable with SQL statement just like a table such as....
+    // SELECT name, album, track FROM artist_list WHERE title = "Go Your Own Way"
+
+    public List<SongArtist> querySongInfoViewNonOptimized(String title) {
+        StringBuilder sb = new StringBuilder(QUERY_VIEW_SONG_INFO);
+        sb.append(title);
+        sb.append("\"");
+
+        System.out.println(sb.toString());
+
+        try (Statement statement = conn.createStatement(); // statement objects are compiled every time a query is processed.
+             // there are more efficient ways to build SQL statements and to also make them less susceptible to hacking
+             ResultSet results = statement.executeQuery(sb.toString())) {
+
+            List<SongArtist> songArtists = new ArrayList<>();
             while(results.next()) {
                 SongArtist songArtist = new SongArtist();
                 songArtist.setArtistName(results.getString(1));
@@ -207,26 +322,36 @@ public class Datasource {
             }
 
             return songArtists;
+
         } catch(SQLException e) {
             System.out.println("Query failed: " + e.getMessage());
             return null;
         }
-    }
+    } // uses regular Statement class
 
-    public void querySongsMetadata() {
-        String sql = "SELECT * FROM " + TABLE_SONGS;
+    public List<SongArtist> querySongInfoView(String title) {
 
-        try (Statement statement = conn.createStatement();
-             ResultSet results = statement.executeQuery(sql)) {
+        try {
+            querySongInfoView.setString(1, title); // PreparedStatement class is subclass of Statement class
+            // so can execute the same methods
+            ResultSet results = querySongInfoView.executeQuery();
+            // don't ahve to worry about closing ResultSet when using PreparedStatements. When closing the PreparedStatement
+            // whatever ResultSet is active will also be closed
 
-            ResultSetMetaData meta = results.getMetaData();
-            int numColumns = meta.getColumnCount();
-            for(int i=1; i<= numColumns; i++) {
-                System.out.format("Column %d in the songs table is names %s\n",
-                        i, meta.getColumnName(i));
+            List<SongArtist> songArtists = new ArrayList<>();
+            while (results.next()) {
+                SongArtist songArtist = new SongArtist();
+                songArtist.setArtistName(results.getString(1));
+                songArtist.setAlbumName(results.getString(2));
+                songArtist.setTrack(results.getInt(3));
+                songArtists.add(songArtist);
             }
-        } catch(SQLException e) {
+
+            return songArtists;
+
+        } catch (SQLException e) {
             System.out.println("Query failed: " + e.getMessage());
+            return null;
         }
     }
 
--- Main.java
+++ Main.java
@@ -33,18 +33,38 @@ public class Main {
 
 
         List<SongArtist> songArtists = datasource.queryArtistsForSong("Heartless", Datasource.ORDER_BY_ASC);
-        if(songArtists == null){
+//        // note the quotes "Heartless". the \" negates the "" as part of this passed content
+//        if(songArtists == null){
+//            System.out.println("Couldn't find the artist for the song");
+//            return;
+//        }
+//
+//        for(SongArtist artist : songArtists){
+//            System.out.println("Artist name = " + artist.getArtistName() + ". Album name  = " + artist.getAlbumName() +
+//                    ". Track = " + artist.getTrack());
+//        }
+//
+//        datasource.querySongsMetadata();
+
+//        int count = datasource.getCount(Datasource.TABLE_SONGS); // there is a lot of separation going on here to make it
+//        // so we don't really need to know we are connecting to a database here from a code perspective... datasource object
+//        // created and connection established. called getCount method to return int. Passing field from Datasource class
+//        // who's String value is 'songs'. This returns the number of songs in the songs table
+//        System.out.println("Number of songs is: " + count);
+
+//        datasource.createViewForSongArtists();
+
+        songArtists = datasource.querySongInfoView("Go Your Own Way");
+        if(songArtists.isEmpty()) { // checking for empty actually better than checking for null
             System.out.println("Couldn't find the artist for the song");
             return;
         }
 
-        for(SongArtist artist : songArtists){
-            System.out.println("Artist name = " + artist.getArtistName() + ". Album name  = " + artist.getAlbumName() +
-                    ". Track = " + artist.getTrack());
+        for(SongArtist artist : songArtists) {
+            System.out.println("FROM VIEW - Artist name = " + artist.getArtistName() +
+                    " Album name = " + artist.getAlbumName() +
+                    " Track number = " + artist.getTrack());
         }
-
-        datasource.querySongsMetadata();
-
 
         datasource.close();
     }
